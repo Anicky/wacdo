5ff28c0fb050438f9bcfa3863923cdbb27be0cde
Anicky <jeremie.jalouzet@gmail.com>
2026-01-08 11:28:47 +0100

    feat: add Gin

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/status", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "OK",
+		})
+	})
+
+	// Listens on 0.0.0.0:8080 by default
+	err := router.Run()
+
+	if err != nil {
+		log.Fatal("Failed to run router.")
+	}
 }
